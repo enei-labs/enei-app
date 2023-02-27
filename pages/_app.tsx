330e5bae4bf8f7548375c21b60157225525cbde7
Jay-Chou <at7211@gmail.com>
2023-02-27 14:26:59 +0800

    feat: add progress bar while route directing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
+import Router from "next/router";
 import theme from "@config/theme";
 import { AuthProvider } from "@core/context/auth";
 import client from "@core/graphql";
@@ -10,6 +11,8 @@ import { ReactElement, ReactNode } from "react";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Snackbar from "@components/Snackbar";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,6 +25,10 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const component = getLayout(<Component {...pageProps} />);
+
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
