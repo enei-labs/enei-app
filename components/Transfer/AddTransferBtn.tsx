e255d59fe2a16a0d8be8948f6ed50c4e26f90c8b
Jay-Chou <at7211@gmail.com>
2023-03-27 00:22:20 +0800

    Add AddTransferBtn, TransferPage, and components to the Transfer page

---
+++ components/Transfer/AddTransferBtn.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+
+const AddTransferBtn = () => {
+  return (
+    <Button startIcon={<AddIcon />} onClick={() => {}}>
+      新增發電業
+    </Button>
+  );
+};
+
+export default AddTransferBtn;
