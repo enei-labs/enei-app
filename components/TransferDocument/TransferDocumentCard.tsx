8aa6e6a71be810d1f294a52536ce0381b54345b7
Jay-Chou <at7211@gmail.com>
2023-04-14 17:26:17 +0800

    feat(transfer-document: add removeTransferDocument

--- components/TransferDocument/TransferDocumentCard.tsx
+++ components/TransferDocument/TransferDocumentCard.tsx
@@ -2,6 +2,12 @@ import { IconBtn } from "@components/Button";
 import { TransferDocument } from "@core/graphql/types";
 import { Box, Card, Typography } from "@mui/material";
 import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import { useRemoveTransferDocument } from "@utils/hooks";
+import { toast } from "react-toastify";
+
+const DialogAlert = dynamic(() => import("@components/DialogAlert"));
 
 interface TransferDocumentProps {
   transferDocument: TransferDocument;
@@ -9,22 +15,49 @@ interface TransferDocumentProps {
 
 function TransferDocumentCard(props: TransferDocumentProps) {
   const { transferDocument } = props;
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [removeTransferDocument, { loading }] = useRemoveTransferDocument();
+
   return (
-    <Card sx={{ p: "36px" }}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4">{`${transferDocument.number}(${transferDocument.name})`}</Typography>
-        <Box sx={{ display: "flex" }}>
-          {/* <EditCompanyContractBtn companyContract={companyContract} /> */}
-          <IconBtn icon={<DeleteOutlined />} onClick={() => {}} />
+    <>
+      <Card sx={{ p: "36px" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h4">{`${transferDocument.number}(${transferDocument.name})`}</Typography>
+          <Box sx={{ display: "flex" }}>
+            {/* <EditCompanyContractBtn companyContract={companyContract} /> */}
+            <IconBtn
+              icon={<DeleteOutlined />}
+              onClick={() => setOpenDeleteDialog(true)}
+            />
+          </Box>
         </Box>
-      </Box>
-    </Card>
+      </Card>
+      {openDeleteDialog ? (
+        <DialogAlert
+          open={openDeleteDialog}
+          title={"刪除轉供合約"}
+          content={"是否確認要刪除轉供合約？"}
+          onConfirm={() => {
+            removeTransferDocument({
+              variables: { id: transferDocument.id },
+              onCompleted: () => {
+                toast.success("刪除成功");
+                setOpenDeleteDialog(false);
+              },
+            });
+          }}
+          onClose={() => {
+            setOpenDeleteDialog(false);
+          }}
+        />
+      ) : null}
+    </>
   );
 }
 
