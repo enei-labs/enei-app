8ef6eddaa300a568ef0adfdd1b53d9a3717eaca0
Jay-Chou <at7211@gmail.com>
2023-06-20 23:06:36 +0800

    refactor(field): form components

--- components/Controller/FieldsController.tsx
+++ components/Controller/FieldsController.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import {
   InputAutocomplete,
   InputAutocompleteList,
@@ -9,11 +10,21 @@ import { FieldConfig } from "@core/types";
 import { Form } from "@core/types/fieldController";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { get } from "lodash";
-import React from "react";
 import { Controller } from "react-hook-form";
 import UploadDocBox from "../UploadDocBox";
 
+type FieldType =
+  | "NUMBER"
+  | "EMAIL"
+  | "TEXT"
+  | "PASSWORD"
+  | "TEXTAREA"
+  | "DATE"
+  | "DATE_TEXTFILE"
+  | "SINGLE_SELECT"
+  | "MULTIPLE_SELECT"
+  | "FILE"
+  | "COMPONENT";
 interface FieldsControllerProps {
   configs: FieldConfig[];
   form: Form;
@@ -25,12 +36,66 @@ const FieldsController: React.FC<FieldsControllerProps> = ({
 }) => {
   const { control, errors } = form;
 
+  const renderInputField = (fieldProps: any, type: FieldType, item: any) => {
+    const { name } = fieldProps.field;
+    const helperText = errors[name]?.message;
+
+    switch (type) {
+      case "NUMBER":
+      case "EMAIL":
+      case "TEXT":
+      case "PASSWORD":
+        return (
+          <InputText {...item} {...fieldProps.field} helperText={helperText} />
+        );
+
+      case "TEXTAREA":
+        return (
+          <InputTextarea
+            {...item}
+            {...fieldProps.field}
+            helperText={helperText}
+          />
+        );
+
+      case "DATE":
+      case "DATE_TEXTFILE":
+        return (
+          <InputDate {...item} {...fieldProps.field} helperText={helperText} />
+        );
+
+      case "SINGLE_SELECT":
+        return (
+          <InputAutocomplete
+            {...item}
+            {...fieldProps.field}
+            helperText={helperText}
+          />
+        );
+
+      case "MULTIPLE_SELECT":
+        return (
+          <InputAutocompleteList
+            {...item}
+            {...fieldProps.field}
+            helperText={helperText}
+          />
+        );
+
+      case "FILE":
+        return (
+          <UploadDocBox label={item.label} {...item} {...fieldProps.field} />
+        );
+
+      default:
+        return <></>;
+    }
+  };
+
   return (
     <>
       {configs.map((item) => {
         const { type, name, hint, component: Component, ...props } = item;
-
-        const helperText = get(errors, `${name}.message`);
 
         const controller =
           type === "COMPONENT" && Component ? (
@@ -39,71 +104,20 @@ const FieldsController: React.FC<FieldsControllerProps> = ({
             <Controller
               name={name}
               control={control}
-              render={({ field }) => {
-                switch (type) {
-                  case "NUMBER":
-                  case "EMAIL":
-                  case "TEXT":
-                  case "PASSWORD":
-                    return (
-                      <InputText {...item} {...field} helperText={helperText} />
-                    );
-
-                  case "TEXTAREA":
-                    return (
-                      <InputTextarea
-                        {...item}
-                        {...field}
-                        helperText={helperText}
-                      />
-                    );
-
-                  case "DATE":
-                  case "DATE_TEXTFILE":
-                    return (
-                      <InputDate {...item} {...field} helperText={helperText} />
-                    );
-
-                  case "SINGLE_SELECT":
-                    return (
-                      <InputAutocomplete
-                        {...item}
-                        {...field}
-                        helperText={helperText}
-                      />
-                    );
-
-                  case "MULTIPLE_SELECT":
-                    return (
-                      <InputAutocompleteList
-                        {...item}
-                        {...field}
-                        helperText={helperText}
-                      />
-                    );
-
-                  case "FILE":
-                    return (
-                      <UploadDocBox label={item.label} {...item} {...field} />
-                    );
-
-                  default:
-                    return <></>;
-                }
-              }}
+              render={(fieldProps) =>
+                renderInputField(fieldProps, type as FieldType, item)
+              }
             />
           );
 
         return (
           <Box key={name} width={1}>
             {controller}
-            <Typography
-              variant="hint"
-              color="text.secondary"
-              mt={hint ? "5px" : ""}
-            >
-              {hint}
-            </Typography>
+            {hint && (
+              <Typography variant="caption" color="text.secondary" mt="5px">
+                {hint}
+              </Typography>
+            )}
           </Box>
         );
       })}
@@ -111,4 +125,4 @@ const FieldsController: React.FC<FieldsControllerProps> = ({
   );
 };
 
-export default FieldsController;
+export default memo(FieldsController);
--- utils/hooks/useValidatedForm.ts
+++ utils/hooks/useValidatedForm.ts
@@ -5,21 +5,33 @@ import type { UseFormReturn } from 'react-hook-form'
 import { FieldValues, useForm, UseFormProps } from 'react-hook-form'
 import * as yup from 'yup'
 
+interface Validations {
+  [key: string]: any;
+}
+
 const useValidatedForm = <TFieldValues extends FieldValues = FieldValues, TContext = any>(
   fieldConfigs: FieldConfig[] = [],
   useFormProps?: UseFormProps<TFieldValues, TContext>,
 ): UseFormReturn<TFieldValues, TContext> => {
+
+  // Create a validation schema using yup based on the field configurations passed
   const schema = useMemo(
     () =>
       yup.object({
-        ...fieldConfigs.reduce((prev, curr) => {
-          if (curr && curr.validated) prev[curr.name] = curr.validated
-          return prev
-        }, {} as { [key: string]: any }),
+        ...fieldConfigs.reduce((prev: Validations, curr: FieldConfig) => {
+          // If the current field has validation, add it to the schema
+          return curr && curr.validated
+            ? { ...prev, [curr.name]: curr.validated }
+            : prev
+        }, {}),
       }),
     [fieldConfigs],
   )
 
+  // If there is an issue in schema creation, you might want to handle it here.
+  // For example, you could log it or show a user-friendly message.
+
+  // Return the form hooks with the resolver for schema validation
   return useForm({ ...useFormProps, resolver: yupResolver(schema) })
 }
 
