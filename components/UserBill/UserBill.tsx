d7f6dc9735e2d9e4d5d3f8ac1891ca38392ec9e5
Jay-Chou <at7211@gmail.com>
2023-07-11 21:42:10 +0800

    feat(user-bill): implement user-bill download

--- components/UserBill/UserBill.tsx
+++ components/UserBill/UserBill.tsx
@@ -9,7 +9,7 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import { useMemo } from "react";
+import { forwardRef, useMemo } from "react";
 
 const styles = {
   container: {
@@ -46,7 +46,7 @@ interface UserBillProps {
 }
 
 /** @TODO check details */
-export function UserBill(props: UserBillProps) {
+const UserBill = forwardRef((props: UserBillProps, ref) => {
   const { userBill, fee } = props;
   console.log({ userBill });
 
@@ -55,88 +55,109 @@ export function UserBill(props: UserBillProps) {
   }, [userBill]);
 
   return (
-    <Box sx={styles.container}>
+    <Box sx={styles.container} ref={ref}>
       <Box display="flex" justifyContent="space-between">
         <Box>logo</Box>
         <Box>繳費通知單</Box>
       </Box>
-      <Box>
-        <Typography variant="h6">{userBill.contactName}</Typography>
+      <Box sx={{ margin: "46px 84px" }}>
+        <Box sx={{ display: "flex", columnGap: "12px" }}>
+          <Typography variant="h6">{userBill.contactName}</Typography>
+          <Typography variant="h6" sx={{ fontWeight: 500 }}>
+            聯絡人
+          </Typography>
+        </Box>
         <Typography variant="body4">{userBill.address}</Typography>
       </Box>
-      <Box>
-        <Grid container columnGap={"8px"}>
-          <Grid item sm={4}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Typography sx={styles.boxTitle}>應繳金額</Typography>
-            </Box>
-            <Box sx={styles.box}>
-              <Typography variant="h5" sx={{ color: "#009688" }}>
-                {"2022.04.20"}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item sm={4}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Typography sx={styles.boxTitle}>繳費期限</Typography>
-            </Box>
-            <Box sx={styles.box}>
-              <Typography variant="h5" sx={{ color: "#009688" }}>
-                {userBill.estimatedBillDeliverDate}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item sm={4}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Typography sx={styles.boxTitle}>匯款資訊</Typography>
-            </Box>
-            <Box sx={styles.box}>
+      <Grid container spacing={"8px"}>
+        <Grid item sm={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography sx={styles.boxTitle}>應繳金額</Typography>
+          </Box>
+          <Box sx={styles.box}>
+            <Typography variant="h5" sx={{ color: "#009688" }}>
+              {"2022.04.20"}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item sm={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography sx={styles.boxTitle}>繳費期限</Typography>
+          </Box>
+          <Box sx={styles.box}>
+            <Typography variant="h5" sx={{ color: "#009688" }}>
+              {userBill.estimatedBillDeliverDate}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item sm={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography sx={styles.boxTitle}>匯款資訊</Typography>
+          </Box>
+          <Box sx={styles.box}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                columnGap: "4px",
+              }}
+            >
               <Box
                 sx={{
                   display: "flex",
-                  flexDirection: "column",
-                  columnGap: "4px",
+                  justifyContent: "space-between",
+                  flex: "1 4",
                 }}
               >
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flex: "1 4",
-                  }}
-                >
-                  <Typography variant="menuType">銀行</Typography>
-                  <Typography variant="menuType">
-                    {`${userBill.recipientAccount.bankCode}`}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flex: "1 4",
-                  }}
-                >
-                  <Typography variant="menuType">帳號</Typography>
-                  <Typography variant="menuType">
-                    {userBill.recipientAccount.account}
-                  </Typography>
-                </Box>
+                <Typography variant="menuType">銀行</Typography>
+                <Typography variant="menuType">
+                  {`${userBill.recipientAccount.bankCode}`}
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flex: "1 4",
+                }}
+              >
+                <Typography variant="menuType">帳號</Typography>
+                <Typography variant="menuType">
+                  {userBill.recipientAccount.account}
+                </Typography>
               </Box>
             </Box>
-          </Grid>
+          </Box>
         </Grid>
-      </Box>
-      <Box>
-        <Typography textAlign={"center"}>轉供資料與明細</Typography>
-        <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+      </Grid>
+      <Box marginTop={"12px"}>
+        <Typography
+          sx={{ fontSize: "12px", fontWeight: 700 }}
+          textAlign={"center"}
+        >
+          轉供資料與明細
+        </Typography>
+        <Table
+          sx={{ minWidth: 700, marginTop: "4px", border: "solid 1px #000" }}
+          aria-label="spanning table"
+        >
           <TableHead>
             <TableRow>
-              <TableCell align="center">電號</TableCell>
-              <TableCell align="center">發電廠</TableCell>
-              <TableCell align="center">轉供度數（度）</TableCell>
-              <TableCell align="center">每度電單價（元/kWh）</TableCell>
-              <TableCell align="center">電費（元）</TableCell>
+              <TableCell align="center" sx={styles.itemTitle}>
+                電號
+              </TableCell>
+              <TableCell align="center" sx={styles.itemTitle}>
+                發電廠
+              </TableCell>
+              <TableCell align="center" sx={styles.itemTitle}>
+                轉供度數（度）
+              </TableCell>
+              <TableCell align="center" sx={styles.itemTitle}>
+                每度電單價（元/kWh）
+              </TableCell>
+              <TableCell align="center" sx={styles.itemTitle}>
+                電費（元）
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -244,22 +265,27 @@ export function UserBill(props: UserBillProps) {
               <TableCell colSpan={2}>Total</TableCell>
               <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
             </TableRow> */}
+            <TableRow>
+              <TableCell colSpan={5} sx={{ padding: "3px" }}>
+                <Box sx={styles.contentText}>
+                  本期計費期間：2022.03.01-2022.03.31 1.
+                  轉供度數以台電公司寄給XXXX（售電業公司名稱）之「台灣電力股份有限公司繳費通知單」所載之代輸電力度數為準。
+                  2.
+                  如用戶逾期繳納金額，按中華郵政股份一年期定期儲金利率固定牌告利率加年息2.5%，計算遲延利息。3.
+                  如用戶逾期繳納金額累計達1個月，並經定期催告仍無於一定期限內給付，依雙方簽訂契約之損害賠償責任辦理。4.
+                  電費奉准以元為單位，不及一元者四捨五入計算。 5.
+                  憑證每達一千度累積電量，憑證中心核發一張，憑證之數量以憑證中心每個月核發之數量為準。
+                  6. 憑證附隨電能一併出售予用戶，不另外計價 。 7.
+                  本繳費憑證各項金額數目係由機器印出，如發現非機器列印或有塗改字跡，概屬無效。
+                </Box>
+              </TableCell>
+            </TableRow>
           </TableBody>
         </Table>
-        <Box sx={{ padding: "3px" }}>
-          <Box sx={styles.contentText}>
-            本期計費期間：2022.03.01-2022.03.31 1.
-            轉供度數以台電公司寄給XXXX（售電業公司名稱）之「台灣電力股份有限公司繳費通知單」所載之代輸電力度數為準。
-            2.
-            如用戶逾期繳納金額，按中華郵政股份一年期定期儲金利率固定牌告利率加年息2.5%，計算遲延利息。3.
-            如用戶逾期繳納金額累計達1個月，並經定期催告仍無於一定期限內給付，依雙方簽訂契約之損害賠償責任辦理。4.
-            電費奉准以元為單位，不及一元者四捨五入計算。 5.
-            憑證每達一千度累積電量，憑證中心核發一張，憑證之數量以憑證中心每個月核發之數量為準。
-            6. 憑證附隨電能一併出售予用戶，不另外計價 。 7.
-            本繳費憑證各項金額數目係由機器印出，如發現非機器列印或有塗改字跡，概屬無效。
-          </Box>
-        </Box>
       </Box>
     </Box>
   );
-}
+});
+
+UserBill.displayName = "UserBill";
+export default UserBill;
--- components/UserBill/UserBillDownloadDialog.tsx
+++ components/UserBill/UserBillDownloadDialog.tsx
@@ -1,6 +1,8 @@
-import { UserBill } from "@components/UserBill/UserBill";
+import UserBill from "@components/UserBill/UserBill";
 import { Fee, UserBill as UserBillType } from "@core/graphql/types";
-import { Box, Dialog } from "@mui/material";
+import { Box, Button, Dialog } from "@mui/material";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 interface UserBillDownloadDialogProps {
   isOpenDialog: boolean;
@@ -11,13 +13,32 @@ interface UserBillDownloadDialogProps {
 
 export function UserBillDownloadDialog(props: UserBillDownloadDialogProps) {
   const { isOpenDialog, onClose, userBill, fee } = props;
+  const componentRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
-    <Dialog open={isOpenDialog} onClose={onClose} sx={{           
-      '& .MuiDialog-paper': {
-      maxWidth: 800,
-    } }}>
-      <Box sx={{ backgroundColor: "primary.light", padding: '32px 24px',}}>
-        <UserBill userBill={userBill} fee={fee} />
+    <Dialog
+      open={isOpenDialog}
+      onClose={onClose}
+      sx={{
+        "& .MuiDialog-paper": {
+          maxWidth: 800,
+        },
+      }}
+    >
+      <Box sx={{ backgroundColor: "primary.light", padding: "32px 24px" }}>
+        <UserBill ref={componentRef} userBill={userBill} fee={fee} />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "8px",
+          }}
+        >
+          <Button onClick={handlePrint}>匯出 PDF</Button>
+        </Box>
       </Box>
     </Dialog>
   );
