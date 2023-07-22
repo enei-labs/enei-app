f2d3d243d2948f5adba73c3e31c02f21beac3cac
Jay-Chou <at7211@gmail.com>
2023-07-22 21:43:10 +0800

    fix(user-contract): resolve UserContract create api

--- utils/hooks/mutations/useCreateCompanyContract.ts
+++ utils/hooks/mutations/useCreateCompanyContract.ts
@@ -1,31 +1,13 @@
-import { CompanyContract, CompanyContractPage, CreateCompanyContractInput } from '@core/graphql/types';
+import { CompanyContract, CreateCompanyContractInput } from '@core/graphql/types';
 import useMutation from '../useMutation';
 import { CREATE_COMPANY_CONTRACT } from '@core/graphql/mutations';
-import { COMPANY_CONTRACT_FIELDS } from '@core/graphql/fragment';
+import { COMPANY_CONTRACTS } from '@core/graphql/queries';
 
 export const useCreateCompanyContract = () => {
   return useMutation<{ createCompanyContract: CompanyContract }, { input: CreateCompanyContractInput }>(
     CREATE_COMPANY_CONTRACT, {
-      update(cache, { data }) {
-        if (data && data.createCompanyContract.__typename === 'CompanyContract') {
-          cache.modify({
-            fields: {
-              companyContracts(companyContractPage: CompanyContractPage) {
-                const newCompanyContract = cache.writeFragment({
-                  data: data.createCompanyContract,
-                  fragment: COMPANY_CONTRACT_FIELDS
-                });
-
-                return {
-                  total: companyContractPage.total + 1,
-                  list: [newCompanyContract, ...companyContractPage.list],
-                };
-              }
-            },
-            broadcast: false,
-          });
-        }
-      }
+      /** @need refactor */
+      refetchQueries: [COMPANY_CONTRACTS]
     }
   )
 }
--- utils/hooks/mutations/useCreateUserContract.ts
+++ utils/hooks/mutations/useCreateUserContract.ts
@@ -1,31 +1,13 @@
-import { USER_CONTRACT_FIELDS } from '@core/graphql/fragment';
 import useMutation from '../useMutation';
 import { CREATE_USER_CONTRACT } from '@core/graphql/mutations';
-import { CreateUserContractInput, UserContract, UserContractPage } from '@core/graphql/types';
+import { CreateUserContractInput, UserContract } from '@core/graphql/types';
+import { USER_CONTRACTS } from '@core/graphql/queries';
 
 export const useCreateUserContract = () => {
   return useMutation<{ createUserContract: UserContract }, { userId: string, input: CreateUserContractInput }>(
     CREATE_USER_CONTRACT, {
-      update(cache, { data }) {
-        if (data && data.createUserContract.__typename === 'UserContract') {
-          cache.modify({
-            fields: {
-              userContracts(userContractPage: UserContractPage) {
-                const newUserContract = cache.writeFragment({
-                  data: data.createUserContract,
-                  fragment: USER_CONTRACT_FIELDS
-                });
-
-                return {
-                  total: userContractPage.total + 1,
-                  list: [newUserContract, ...userContractPage.list],
-                };
-              }
-            },
-            broadcast: false,
-          });
-        }
-      }
+      /** @need refactor */
+      refetchQueries: [USER_CONTRACTS]
     }
   )
 }
