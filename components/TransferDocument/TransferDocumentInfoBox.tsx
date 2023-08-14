d475c29087986c3b2b2a4ffdd21b36d9a084fa18
Jay-Chou <at7211@gmail.com>
2023-08-15 00:26:39 +0800

    fix

---
+++ components/TransferDocument/TransferDocumentInfoBox.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+/** @TODO  */
+function TransferDocumentInfoBox() {
+  return (
+    <Box display={"flex"} flexDirection="column" rowGap="24px">
+      <Box display={"flex"} gap="8px" flexWrap={"wrap"}>
+        {/* {fields.map((item, index) => {
+        return (
+          <Chip */}
+      </Box>
+    </Box>
+  );
+}
