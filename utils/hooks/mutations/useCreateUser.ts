7516344b94fd5c4e99e6234aaf865888a2c69a06
Jay-Chou <at7211@gmail.com>
2024-03-16 18:20:02 +0800

    feat(utils/hooks/mutations): enhance cache update logic for user creation and removal
    - Integrate Dashboard updates into useCreateUser and useRemoveUser hooks.
    - Adjust cache writeQuery calls to include type specifications for better type safety.
    - Add DASHBOARD query to both hooks for synchronizing dashboard data with user operations.

--- utils/hooks/mutations/useCreateUser.ts
+++ utils/hooks/mutations/useCreateUser.ts
@@ -1,16 +1,17 @@
-import { CreateUserInput, User, UserPage } from "@core/graphql/types";
+import { CreateUserInput, Dashboard, User, UserPage } from "@core/graphql/types";
 import { CREATE_USER } from "@core/graphql/mutations";
 import useMutation from "../useMutation";
-import { USERS } from "@core/graphql/queries";
+import { DASHBOARD, USERS } from "@core/graphql/queries";
 
 export const useCreateUser = () => {
   return useMutation<{ createUser: User }, { input: CreateUserInput }>(CREATE_USER, {
     update(cache, { data }) {
       if (data?.createUser?.__typename === 'User') {
         const existingUsers = cache.readQuery<{ users: UserPage }>({ query: USERS });
+        const existingDashboard = cache.readQuery<{ dashboard: Dashboard }>({ query: DASHBOARD });
 
         if (existingUsers) {
-          cache.writeQuery({
+          cache.writeQuery<{ users: UserPage }>({
             query: USERS,
             data: {
               users: {
@@ -20,6 +21,21 @@ export const useCreateUser = () => {
               },
             },
           });
+        }
+
+        if (existingDashboard) {
+          cache.writeQuery<{ dashboard: Dashboard }>({
+            query: DASHBOARD,
+            data: {
+              dashboard: {
+              ...existingDashboard.dashboard,
+                userInfo: {
+                  ...existingDashboard.dashboard.userInfo,
+                  count: existingDashboard.dashboard.userInfo.count + 1,
+                }
+              }
+            }
+          })
         }
       }
     },
--- utils/hooks/mutations/useRemoveUser.ts
+++ utils/hooks/mutations/useRemoveUser.ts
@@ -1,6 +1,6 @@
-import { RemoveUserInput, User, UserPage } from '@core/graphql/types';
+import { Dashboard, RemoveUserInput, User, UserPage } from '@core/graphql/types';
 import useMutation from '../useMutation';
-import { USERS } from '@core/graphql/queries';
+import { DASHBOARD, USERS } from '@core/graphql/queries';
 import { REMOVE_USER } from '@core/graphql/mutations';
 
 export const useRemoveUser = () => {
@@ -8,18 +8,33 @@ export const useRemoveUser = () => {
     update(cache, { data }) {
       if (data?.removeUser?.__typename === 'User') {
         const existingUsers = cache.readQuery<{ users: UserPage }>({ query: USERS });
+        const existingDashboard = cache.readQuery<{ dashboard: Dashboard }>({ query: DASHBOARD });
 
         if (existingUsers) {
-          cache.writeQuery({
+          cache.writeQuery<{ users: UserPage}>({
             query: USERS,
             data: {
               users: {
-                ...existingUsers.users,
-                total: existingUsers.users.total - 1,
-                list: existingUsers.users.list.filter(user => user.id !== data.removeUser.id),
-              },
-            },
+              ...existingUsers.users,
+              total: existingUsers.users.total - 1,
+              list: existingUsers.users.list.filter(user => user.id !== data.removeUser.id),
+            }},
           });
+        }
+
+        if (existingDashboard) {
+          cache.writeQuery<{ dashboard: Dashboard}>({
+            query: DASHBOARD,
+            data: {
+              dashboard: {
+              ...existingDashboard.dashboard,
+                userInfo: {
+                  ...existingDashboard.dashboard.userInfo,
+                  count: existingDashboard.dashboard.userInfo.count - 1,
+                }
+              }
+            }
+          })
         }
       }
     },
