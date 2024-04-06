bfe7ddf77418fc05c3c735ac99711ebcdd8fda39
Jay-Chou <at7211@gmail.com>
2024-04-06 21:26:33 +0800

    feat(utils/hooks/mutations): enhance useCreateTPCBill with query variables
    Enhanced the `useCreateTPCBill` hook to include query variables for `TPC_BILLS` in both `readQuery` and `writeQuery` methods. This update allows for specifying `offset`, `limit`, and `transferDocumentId` when querying and updating the cache, improving the precision and efficiency of cache operations.

--- utils/hooks/mutations/useCreateTPCBill.ts
+++ utils/hooks/mutations/useCreateTPCBill.ts
@@ -4,17 +4,28 @@ import useMutation from '../useMutation';
 import { CREATE_TPC_BILL } from '@core/graphql/mutations';
 import { TPC_BILLS } from '@core/graphql/queries';
 
-/** @TODO cache issue */
 export const useCreateTPCBill = () => {
   return useMutation<{ createTPCBill: TpcBill }, { input: CreateTpcBillInput }>(
     CREATE_TPC_BILL, {
-      update(cache, { data }) {
+      update(cache, { data }, { variables }) {
         if (data?.createTPCBill?.__typename === 'TPCBill') {
-          const existingTpcBills = cache.readQuery<{ tpcBills: TpcBillPage }>({ query: TPC_BILLS });
+          const existingTpcBills = cache.readQuery<{ tpcBills: TpcBillPage }>({
+            query: TPC_BILLS,
+            variables: {
+              offset: 0,
+              limit: 10,
+              transferDocumentId: variables?.input.transferDocumentId,
+            },
+          });
 
           if (existingTpcBills) {
             cache.writeQuery({
               query: TPC_BILLS,
+              variables: {
+                offset: 0,
+                limit: 10,
+                transferDocumentId: variables?.input.transferDocumentId,
+              },
               data: {
                 tpcBills: {
                   ...existingTpcBills.tpcBills,
