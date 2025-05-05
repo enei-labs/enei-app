ef67cdf1138be37dc79380d4103024b370dd6d58
Jay-Chou <42150440+at7211@users.noreply.github.com>
2025-05-05 16:02:42 +0800

    feat(InputDate): Enhance date input component with validation and improved label handling
    - Added validation to ensure the date is valid before formatting it to ISO string.
    - Updated label rendering to include asterisk for required fields and improved layout using Box and Typography components.
    - Refactored label handling in DatePicker to enhance user experience and maintain consistency.

--- components/Input/InputDate.tsx
+++ components/Input/InputDate.tsx
@@ -5,7 +5,17 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { formatDateTime } from "@utils/format";
 import { forwardRef } from "react";
-import InputText from "@components/Input/InputText";
+import { Box, Typography } from "@mui/material";
+
+const style = {
+  text: [
+    (theme: any) => ({
+      display: "flex",
+      gap: "10px",
+      color: theme.palette.text.primary,
+    }),
+  ],
+};
 
 interface InputDateProps
   extends Omit<DatePickerProps<Date>, "value" | "onChange"> {
@@ -19,27 +29,42 @@ const InputDate = forwardRef<HTMLDivElement, InputDateProps>((props, ref) => {
   const { label, value, required, helperText, onChange, ...otherProps } = props;
 
   const handleChange = (newValue: Date | null, keyboardInputValue?: string) => {
-    let formattedValue = newValue ? formatDateTime(newValue.toISOString()) : "";
+    // Check if newValue is a valid date before trying to use toISOString()
+    let formattedValue = "";
+    
+    if (newValue && !isNaN(newValue.getTime())) {
+      formattedValue = formatDateTime(newValue.toISOString());
+    }
+    
     onChange?.(newValue, formattedValue);
   };
+
+
+  const labelName = (
+    <Box sx={style.text}>
+      {label}
+      {required && <Typography color="error">*</Typography>}
+    </Box>
+  );
+
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={zhTW}>
       <DatePicker
         {...otherProps}
-        label={label}
+        label={label && labelName}
         value={value ? new Date(value) : null}
         onChange={(newValue) => handleChange(newValue)}
-        slots={{
-          textField: (params: any) => (
-            <InputText
-              {...params}
-              ref={undefined}
-              label={label}
-              required={required}
-              helperText={helperText}
-            />
-          ),
+        slotProps={{
+          textField: {
+            fullWidth: true,
+            label: label && labelName,
+            helperText,
+            required: undefined,
+            InputLabelProps: {
+              shrink: true,
+            },
+          }
         }}
       />
     </LocalizationProvider>
